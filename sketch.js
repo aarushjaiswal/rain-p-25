c9d0c38a1fe092fb0e8f392bdad74aa57421af16
aarushjaiswal <64395032+aarushjaiswal@users.noreply.github.com>
2020-05-23 17:51:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,13 +5,13 @@ var rain=[];
 function setup() {
   createCanvas(1500,800);
   for (var i = 0;i<800;i++){
-    drops = new Rain(random(0,1500),0);
+    drops = new Rain(random(0,1500),random(0,800));
     rain.push(drops);
   }
 }
 
 function draw() {
-  background(255,255,255);
+  background(0);
   for (var i = 0;i<800;i++){
     rain[i].display()
     rain[i].fall()
